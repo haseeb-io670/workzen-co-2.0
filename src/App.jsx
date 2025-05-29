ae73cc894f0b5a3f3ff30dc859f308d7d70c6462
Aliyan-12 <alyannasir225@gmail.com>
2025-05-30 00:46:03 +0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,17 @@ import { useState } from 'react'
 import './App.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/index';
+import Header from './components/layout/header.jsx'
+import Footer from './components/layout/footer.jsx'
 
 function App() {
-  return <RouterProvider router={router} />
+  return (
+    <>
+      <Header />
+      <RouterProvider router={router} />
+      <Footer />
+    </>
+  );
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,9 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
-import Header from './components/layout/header.jsx'
-import Footer from './components/layout/footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Header />
     <App />
-    <Footer />
   </React.StrictMode>,
 )
